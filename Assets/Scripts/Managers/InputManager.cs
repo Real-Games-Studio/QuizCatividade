631a31a3ecfe1d6b7e49f24111e96a6b0646985b
MatheusSMA <matheusjut@gmail.com>
2023-11-27 13:55:45 -0300

    Better input code

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -23,7 +23,7 @@ public class InputManager : MonoBehaviour
             _initialScreenbutton.enabled = false;
         }
 
-        if (RestartSceneInput())
+        if (CheckRestartSceneInput())
         {
             Managers.Instance.SceneManagement.RestartScene();
         }
@@ -37,7 +37,11 @@ public class InputManager : MonoBehaviour
             InputForKeyboard(3, KeyCode.Alpha4);
             InputForKeyboard(4, KeyCode.Alpha5);
 
-            InputForControllerOld();
+            InputForControllerOldNew(0, "Fire1");
+            InputForControllerOldNew(1, "Fire2");
+            InputForControllerOldNew(2, "Fire3");
+            InputForControllerOldNew(3, "Jump");
+            InputForLastButton();
         }
 
         if (Managers.Instance.GameManager.FinalScreen && CheckOldButtonPressed())
@@ -45,8 +49,6 @@ public class InputManager : MonoBehaviour
             Managers.Instance.SceneManagement.RestartScene();
             Managers.Instance.GameManager.FinalScreen = false;
         }
-
-
     }
 
     private bool CheckKeyPressed()
@@ -54,8 +56,7 @@ public class InputManager : MonoBehaviour
         if (Input.GetKey(KeyCode.Alpha1) || Input.GetKey(KeyCode.Alpha2) || Input.GetKey(KeyCode.Alpha3) || Input.GetKey(KeyCode.Alpha4) || Input.GetKey(KeyCode.Alpha5)) return true;
         else return false;
     }
-
-    private bool RestartSceneInput()
+    private bool CheckRestartSceneInput()
     {
         if (Input.GetButton("Fire1"))
         {
@@ -110,73 +111,23 @@ public class InputManager : MonoBehaviour
         }
 
     }
-
-    // private void InputForController(int bntIndex, int controllBntOrder)
-    // {
-    //     if (_buttonsControllers[controllBntOrder].WasPressedThisFrame())
-    //     {
-    //         if (_button[bntIndex].interactable) ExecuteEvents.Execute(_button[bntIndex].gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
-    //     }
-    //     if (_buttonsControllers[controllBntOrder].WasReleasedThisFrame())
-    //     {
-    //         if (_button[bntIndex].interactable)
-    //         {
-    //             _button[bntIndex].onClick.Invoke();
-    //             ExecuteEvents.Execute(_button[bntIndex].gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerUpHandler);
-    //         }
-    //     }
-
-    // }
-    private void InputForControllerOld()
+    private void InputForControllerOldNew(int buttonIndex, string buttonName)
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown(buttonName))
         {
-            if (_button[0].interactable) ExecuteEvents.Execute(_button[0].gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
+            if (_button[buttonIndex].interactable) ExecuteEvents.Execute(_button[buttonIndex].gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
         }
-        if (Input.GetButtonUp("Fire1"))
+        if (Input.GetButtonUp(buttonName))
         {
-            if (_button[0].interactable)
+            if (_button[buttonIndex].interactable)
             {
-                _button[0].onClick.Invoke();
-                ExecuteEvents.Execute(_button[0].gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerUpHandler);
+                _button[buttonIndex].onClick.Invoke();
+                ExecuteEvents.Execute(_button[buttonIndex].gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerUpHandler);
             }
         }
-        if (Input.GetButtonDown("Fire2"))
-        {
-            if (_button[1].interactable) ExecuteEvents.Execute(_button[1].gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
-        }
-        if (Input.GetButtonUp("Fire2"))
-        {
-            if (_button[1].interactable)
-            {
-                _button[1].onClick.Invoke();
-                ExecuteEvents.Execute(_button[1].gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerUpHandler);
-            }
-        }
-        if (Input.GetButtonDown("Fire3"))
-        {
-            if (_button[2].interactable) ExecuteEvents.Execute(_button[2].gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
-        }
-        if (Input.GetButtonUp("Fire3"))
-        {
-            if (_button[2].interactable)
-            {
-                _button[2].onClick.Invoke();
-                ExecuteEvents.Execute(_button[2].gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerUpHandler);
-            }
-        }
-        if (Input.GetButtonDown("Jump"))
-        {
-            if (_button[3].interactable) ExecuteEvents.Execute(_button[3].gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
-        }
-        if (Input.GetButtonUp("Jump"))
-        {
-            if (_button[3].interactable)
-            {
-                _button[3].onClick.Invoke();
-                ExecuteEvents.Execute(_button[3].gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerUpHandler);
-            }
-        }
+    }
+    private void InputForLastButton()
+    {
         if (Input.GetButtonDown("SLT") || Input.GetButtonDown("SRT") || Input.GetButtonDown("SLT2") || Input.GetButtonDown("SRT2") || Input.GetButtonDown("SRT3") || Input.GetButtonDown("Start"))
         {
             if (_button[4].interactable) ExecuteEvents.Execute(_button[4].gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
